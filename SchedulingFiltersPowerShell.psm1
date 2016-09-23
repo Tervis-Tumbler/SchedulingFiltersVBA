af4a72fc7d2914e0398453cefccf09114f0cc3c8
adanlozano <adanlozano11@gmail.com>
2016-09-23 08:45:14 -0400

    Removed uneeded function

--- SchedulingFiltersPowerShell.psm1
+++ SchedulingFiltersPowerShell.psm1
@@ -1,36 +1,6 @@
-function Test-FileLock {
-    [cmdletbinding()]
-    param(
-        [parameter(Mandatory=$true)]
-        [string]
-        $Path
-    )
-
-    $oFile = New-Object System.IO.FileInfo $Path
-
-    if ((Test-Path -Path $Path) -eq $false){
-        return $false
-    }
-
-    try {
-        $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
-        if ($oStream) {
-            $oStream.Close()
-        }
-        $false
-        }
-
-    catch {
-        # file is locked by a process.
-        return $true
-    }
-}
-
 function Export-SchedulingFilterExcelVBA{
     $SourceFile = "C:\Users\alozano\Documents\WindowsPowerShell\Modules\SchedulingFiltersVBA\Worksheets\SchedulingFilters.xlsm"
     $DestinationFolder = "C:\Users\alozano\Documents\WindowsPowerShell\Modules\SchedulingFiltersVBA\Worksheet Export"
 
     Export-ExcelProject -WorkbookPath $SourceFile -OutputPath $DestinationFolder -Verbose
 }
-
-Export-SchedulingFilterExcelVBA
